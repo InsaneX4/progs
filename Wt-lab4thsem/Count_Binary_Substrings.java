90f198afac2fc5d7370b613be179b810e38f2074
InsaneX4 <dibyansu.mishra2003@gmail.com>
2023-04-11 14:56:27 +0530

    u

--- Wt-lab4thsem/Count_Binary_Substrings.java
+++ Wt-lab4thsem/Count_Binary_Substrings.java
@@ -2,6 +2,8 @@ public class Count_Binary_Substrings {
     public static void main(String[] args) {
         // 01 10 1100 0011
         // "00110011"
+        Count_Binary_Substrings obj = new Count_Binary_Substrings();
+        System.out.println(obj.countBinarySubstrings("00110011"));
 
     }
     public int countBinarySubstrings(String s) {
@@ -12,17 +14,19 @@ public class Count_Binary_Substrings {
         int consecutivediff = 0;
 
         for(int i = 0;i<s.length();i++) {
+            if(s.charAt(i) != s.charAt(s.length()-1)){
             if(s.charAt(i) == s.charAt(i+1)) {
                 consecutivesame++;
+                if(consecutivesame != consecutivediff){
+                    count++;
+                }
             }else{
                 consecutivediff++;
                 count++;
-                if(consecutivesame > 0) {
-
-                }
             }
+        }
             
         }
-        return 0; 
+        return count; 
     }
 }
